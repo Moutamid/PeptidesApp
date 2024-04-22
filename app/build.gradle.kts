15b67834452c37c901f8b45ab0bf75f48ead42c5
moutamid <lumianokia188@gmail.com>
2024-04-22 11:49:29 +0500

    commit

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -12,8 +12,8 @@ android {
         applicationId = "com.moutamid.peptidesapp"
         minSdk = 24
         targetSdk = 34
-        versionCode = 1
-        versionName = "1.0"
+        versionCode = 2
+        versionName = "1.1"
         setProperty("archivesBaseName", "RPSA-Peptide Pocket Reference-$versionName")
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
